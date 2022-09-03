e3270686b06caecafe066ccf18fe65c5fa8c157d
knakayama <knakayama.sh@gmail.com>
2022-09-03 11:21:41 +0800

    Add test cases

--- internal/entity/vpc_test.go
+++ internal/entity/vpc_test.go
@@ -3,6 +3,7 @@ package entity
 import (
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/knakayama/dv/test"
 	"github.com/stretchr/testify/assert"
 )
@@ -15,13 +16,21 @@ func setupTest(_ *testing.T) func(_ *testing.T) {
 	}
 }
 
+func TestNewClientError(t *testing.T) {
+	vpc, err := NewVpc(&ec2.Client{})
+
+	assert.NotNil(t, vpc)
+	assert.NotNil(t, err)
+}
+
 func TestNewVpcNoVpc(t *testing.T) {
 	teardownTest := setupTest(t)
 	defer teardownTest(t)
 
-	vpc, _ := NewVpc(NewDefaultClient())
+	vpc, err := NewVpc(NewDefaultClient())
 
 	assert.Nil(t, vpc.Id)
+	assert.Nil(t, err)
 }
 
 func TestNewVpcNoDefaultVpc(t *testing.T) {
@@ -29,9 +38,10 @@ func TestNewVpcNoDefaultVpc(t *testing.T) {
 	defer teardownTest(t)
 	test.MakeVpcs()
 
-	vpc, _ := NewVpc(NewDefaultClient())
+	vpc, err := NewVpc(NewDefaultClient())
 
 	assert.Nil(t, vpc.Id)
+	assert.Nil(t, err)
 }
 
 func TestNewVpcDefaultVpcFound(t *testing.T) {
@@ -39,7 +49,8 @@ func TestNewVpcDefaultVpcFound(t *testing.T) {
 	defer teardownTest(t)
 	test.MakeDefaultVpc()
 
-	vpc, _ := NewVpc(NewDefaultClient())
+	vpc, err := NewVpc(NewDefaultClient())
 
 	assert.NotNil(t, vpc.Id)
+	assert.Nil(t, err)
 }
